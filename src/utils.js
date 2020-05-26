40ac5f692ba214e21effc191863284b4fea971fb
RusuGabriel <rusugabriel13@gmail.com>
2020-05-26 23:57:53 +0300

    added utils.js file

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
 function check() {
     document.getElementById("demo").innerHTML = "That's all folks";
 }
+
+function changeColor() {
+    document.getElementById("demo").style.color = "magenta";
+}
