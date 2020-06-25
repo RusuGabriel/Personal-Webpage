62689bf77e8ba11512d79c72e91136fd31046a71
Gabriel Rusu <rusugabriel13@gmail.com>
2020-06-25 22:42:02 +0300

    add minor improvments to the interface

--- src/utils.js
+++ src/utils.js
@@ -49,7 +49,8 @@ var TxtRotate = function(el, toRotate, period) {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
   
     var that = this;
-    var delta = 200 - Math.random() * 100;
+    // var delta = 200 - Math.random() * 100;
+    let delta  = 150
   
     if (this.isDeleting) { delta /= 2; }
   
